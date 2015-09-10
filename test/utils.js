f327df70105ccc50cbe12a1b40a00e6b8d6b93c9
Chris Breneman <crispy@cluenet.org>
2015-09-10 12:20:02 -0400

    More stuff

---
+++ test/utils.js
@@ -0,0 +1,13 @@
+const expect = require('chai').expect;
+const { runCallMiddleware } = require('../lib/utils');
+
+describe('runCallMiddleware()', function() {
+
+	it('should run pre-middleware until a result is returned', function() {
+		let context = {};
+		return runCallMiddleware()
+	});
+
+});
+
+
