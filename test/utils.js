72fc958a42ce734214075a9a22fc460c987fefc3
Ross Hadden <rosshadden@gmail.com>
2015-09-10 16:36:25 -0400

    Updated linter rules, and solved some lint issues.

--- test/utils.js
+++ test/utils.js
@@ -2,12 +2,8 @@ const expect = require('chai').expect;
 const { runCallMiddleware } = require('../lib/utils');
 
 describe('runCallMiddleware()', function() {
-
 	it('should run pre-middleware until a result is returned', function() {
 		let context = {};
-		return runCallMiddleware()
+		return runCallMiddleware();
 	});
-
 });
-
-
