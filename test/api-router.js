cf7f81c9a5044051f2522a6a27a8da080cb42647
Ross Hadden <rosshadden@gmail.com>
2015-09-16 14:29:46 -0400

    Added so many more tests.

--- test/api-router.js
+++ test/api-router.js
@@ -1,6 +1,7 @@
 const supertest = require('supertest');
 const express = require('express');
 const expect = require('chai').expect;
+const XError = require('xerror');
 const APIRouter = require('../lib/api-router');
 const HTTPRPCInterface = require('../lib/http-rpc-interface');
 
@@ -40,23 +41,86 @@ describe('APIRouter', function() {
 		expect(router.versionRouters[1].interfaces).to.have.length(1);
 	});
 
-	it('should register methods', function(done) {
+	it('#register should support returns', function(done) {
 		router.register({
 			method: 'method'
-		}, (ctx) => {
-			ctx.res.json({ foo: 'bar' });
-		});
-
-		router.register({
-			method: 'method.with.long.name'
-		}, (ctx) => {
-			ctx.res.json({ long: true });
+		}, () => {
+			return 'some result';
 		});
 
 		request.post('/v1/rpc/method')
-			.expect(200, { foo: 'bar' }, () => {
-				request.post('/v1/rpc/method/with/long/name')
-					.expect(200, { long: true }, done);
+			.expect(200, { result: 'some result' }, done);
+	});
+
+	it('#register should support errors', function(done) {
+		router.register({
+			method: 'method'
+		}, () => {
+			throw new Error('some error');
+		});
+
+		request.post('/v1/rpc/method')
+			.expect(200, { error: { code: 'internal_error', message: 'some error' } }, done);
+	});
+
+	it('#register should support segmented method names', function(done) {
+		router.register({
+			method: 'method.with.long.name'
+		}, () => {
+			return { long: true };
+		});
+
+		request.post('/v1/rpc/method/with/long/name')
+			.expect(200, { result: { long: true } }, done);
+	});
+
+	it('should support resolving promises in methods', function(done) {
+		router.register({
+			method: 'resolve'
+		}, () => {
+			return new Promise((resolve) => {
+				setImmediate(() => resolve({ foo: 'bar' }));
+			});
+		});
+
+		request.post('/v1/rpc/resolve')
+			.expect(200, { result: { foo: 'bar' } }, done);
+	});
+
+	it('should support rejecting promises in methods', function(done) {
+		router.register({
+			method: 'reject'
+		}, () => {
+			return new Promise((resolve, reject) => {
+				setImmediate(() => reject(new Error('some error')));
+			});
+		});
+
+		request.post('/v1/rpc/reject')
+			.expect(200, { error: { code: 'internal_error', message: 'some error' } }, done);
+	});
+
+	it('should support XErrors', function(done) {
+		router.register({
+			method: 'not_modified'
+		}, () => {
+			throw new XError(XError.NOT_MODIFIED, 'I\'m afraid I can\'t do that.');
+		});
+
+		router.register({
+			method: 'limit_exceeded'
+		}, () => {
+			return new Promise((resolve, reject) => {
+				setImmediate(() => reject(new XError(XError.LIMIT_EXCEEDED, 'STOP DOING THAT!')));
+			});
+		});
+
+		request.post('/v1/rpc/not_modified')
+			.expect(200, { error: { code: 'not_modified', message: 'I\'m afraid I can\'t do that.' } }, (err) => {
+				if (err) throw err;
+
+				request.post('/v1/rpc/limit_exceeded')
+					.expect(200, { error: { code: 'limit_exceeded', message: 'STOP DOING THAT!' } }, done);
 			});
 	});
 
@@ -67,25 +131,28 @@ describe('APIRouter', function() {
 			ctx.data = { one: 1 };
 		}, (ctx) => {
 			ctx.data.two = 2;
-			ctx.res.json(ctx.data);
+			return ctx.data;
 		});
 
 		let numEnteredMiddlewares = 0;
 		router.register({
 			method: 'method.with.skipped.middleware'
-		}, (ctx) => {
+		}, () => {
 			numEnteredMiddlewares += 1;
-			ctx.res.json({ one: 1 });
-			return true;
-		}, (ctx) => {
+			return { one: 1 };
+		}, () => {
 			numEnteredMiddlewares += 1;
-			ctx.res.json({ two: 2 });
+			return { two: 2 };
 		});
 
 		request.post('/v1/rpc/method/with/middleware')
-			.expect(200, { one: 1, two: 2 }, () => {
+			.expect(200, { result: { one: 1, two: 2 } }, (err) => {
+				if (err) throw err;
+
 				request.post('/v1/rpc/method/with/skipped/middleware')
-					.expect(200, { one: 1 }, () => {
+					.expect(200, { result: { one: 1 } }, (err) => {
+						if (err) throw err;
+
 						expect(numEnteredMiddlewares).to.equal(1);
 						done();
 					});
@@ -99,14 +166,16 @@ describe('APIRouter', function() {
 		newRouter.register({
 			method: 'version-specific',
 			version: 2
-		}, (ctx) => {
-			ctx.res.json({ foo: 'bar' });
+		}, () => {
+			return { foo: 'bar' };
 		});
 
 		request.post('/v1/rpc/version-specific')
-			.expect(500, () => {
+			.expect(500, (err) => {
+				if (err) throw err;
+
 				request.post('/v2/rpc/version-specific')
-					.expect(200, { foo: 'bar' }, done);
+					.expect(200, { result: { foo: 'bar' } }, done);
 			});
 	});
 
@@ -118,11 +187,11 @@ describe('APIRouter', function() {
 		router.register({
 			method: 'method.with.pre.middleware'
 		}, (ctx) => {
-			ctx.res.json({ someProp: ctx.someProp });
+			return { someProp: ctx.someProp };
 		});
 
 		request.post('/v1/rpc/method/with/pre/middleware')
-			.expect(200, { someProp: 'foo' }, done);
+			.expect(200, { result: { someProp: 'foo' } }, done);
 	});
 
 	it('should register post-middleware', function(done) {
@@ -133,12 +202,14 @@ describe('APIRouter', function() {
 
 		router.register({
 			method: 'method.with.post.middleware'
-		}, (ctx) => {
-			ctx.res.json('some response');
+		}, () => {
+			return 'some response';
 		});
 
 		request.post('/v1/rpc/method/with/post/middleware')
-			.expect(200, () => {
+			.expect(200, (err) => {
+				if (err) throw err;
+
 				expect(hasRanMiddleware).to.be.true;
 				done();
 			});
